0b024bd086d10bf4ac388be9dc72b660ef9d5a00
bettaieb med ali <medali.bettaieb@esprit.tn>
2016-09-22 15:26:05 +0200

    client project
    Signed-off-by: bettaieb med ali <medali.bettaieb@esprit.tn>

--- trash-track/trash-track-ejb/src/main/java/tn/esprit/tt/services/CompanyServices.java
+++ trash-track/trash-track-ejb/src/main/java/tn/esprit/tt/services/CompanyServices.java
@@ -1,11 +1,13 @@
 package tn.esprit.tt.services;
 
+import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 
 /**
  * Session Bean implementation class CompanyServices
  */
 @Stateless
+@LocalBean
 public class CompanyServices implements CompanyServicesRemote, CompanyServicesLocal {
 
 	/**
